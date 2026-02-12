eaffebf228bd873682934131a1d76452fdacf7fd
naresh-rn <naresh.ravic@gmail.com>
2026-02-12 19:51:14 +0530

    fixed env variables

--- index.js
+++ index.js
@@ -69,7 +69,11 @@ cloudinary.config({
 const Project = mongoose.model('Project', ProjectSchema);
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "http://localhost:5173"], // Allow React and Vite
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "x-auth-token"] // Matches your App.js header
+}));
 app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
